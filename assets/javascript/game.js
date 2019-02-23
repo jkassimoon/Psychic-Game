02307c125b209e4e496b84607d9fda472c653335
thepickupkitten <jkashmoon@gmail.com>
2019-02-22 21:42:14 -0800

    commit finished HTML CSS and JS

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,7 @@
 var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m","n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var wins = 0;
 var losses = 0;
-var guessesLeft = 5;
+var guessesLeft = 9;
 var lettersTyped = [];
 
 var winsCounter = document.getElementById("wins-counter");
@@ -12,28 +12,35 @@ var userInputLog = document.getElementById("user-input-log");
 var psychicThinking = alphabet[Math.floor(Math.random() * alphabet.length)];
     console.log(psychicThinking);
 
+function resetGame() {
+    lettersTyped = [];
+    guessesLeft = 9;
+    psychicThinking = alphabet[Math.floor(Math.random() * alphabet.length)];
+        console.log(psychicThinking);
+};
+
 document.onkeyup = function(event) {
     var userTry = event.key.toLowerCase();
     
+    if (!alphabet.includes(userTry) || lettersTyped.includes(userTry)) {
+        return;
+    }
+    
     if (userTry === psychicThinking) {
-        lettersTyped = [];
         wins++;
-        guessesLeft = 0;
-        psychicThinking = alphabet[Math.floor(Math.random() * alphabet.length)];
-            console.log(psychicThinking);
+        resetGame();
     }
 
     else {
-        lettersTyped.push(userTry);
-        losses++;
-            if (guessesLeft < 0) {
-                guessesLeft = 9;
-                lettersTyped = "";
+        if (guessesLeft <= 0) {   
+            resetGame();
+            losses++;
+        }
+                
+        else {
+            lettersTyped.push(userTry);
+            guessesLeft--;
             }
-            
-            else {
-                guessesLeft--;
-            };
     }
 
     winsCounter.textContent = "Wins: " + wins;
